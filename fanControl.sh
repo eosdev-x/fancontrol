3d933ff6a661c904cfc48b66146ab5a89da69672
eosdev <401944+eosdev-x@users.noreply.github.com>
2023-02-24 06:41:29 -0800

    Add files via upload

---
+++ fanControl.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+sudo opencorsairlink --device 0 --fan channel=0,mode=0,pwm=50
+sudo opencorsairlink --device 0 --fan channel=1,mode=0,pwm=50
+sudo opencorsairlink --device 0 --fan channel=2,mode=0,pwm=50
+sudo opencorsairlink --device 0 --fan channel=3,mode=0,pwm=50
+sudo opencorsairlink --device 0 --fan channel=4,mode=0,pwm=50
+sudo opencorsairlink --device 0 --fan channel=5,mode=0,pwm=50
